42205dca87204be909f132ce554a9cf6a69c2c32
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2023-01-17 11:36:06 -0300

    Recursos de sistemas móveis

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import * as Battery from "expo-battery";
+import { StyleSheet, Text, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    batteryLevel: null,
+  };
+
+  componentDidMount() {
+    this._subscribe();
+  }
+
+  componentWillUnmount() {
+    this._unsubscribe();
+  }
+
+  async _subscribe() {
+    const batteryLevel = await Battery.getBatteryLevelAsync();
+    this.setState({ batteryLevel });
+    this._subscription = Battery.addBatteryLevelListener(({ batteryLevel }) => {
+      this.setState({ batteryLevel });
+      console.log("batteryLevel changed!", batteryLevel);
+    });
+  }
+
+  _unsubscribe() {
+    this._subscription && this._subscription.remove();
+    this._subscription = null;
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Nível atual da bateria</Text>
+
+        <Text>
+          {this.state.batteryLevel}{" "}
+          <FontAwesome name="battery-4" size={16} color="black" />
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f9f9f9",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titulo: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginVertical: 20,
   },
 });
